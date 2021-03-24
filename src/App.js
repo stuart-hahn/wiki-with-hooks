bc013d69072411e041e1e7d8bd1b384285db48b6
Stuart Hahn <stuart.a.hahn@gmail.com>
2021-03-24 13:45:18 -0400

    create controlled form that updates search  term using useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return <div>Hello</div>
+  const [term, setTerm] = useState('')
+  return (
+    <>
+      <div>Hello</div>
+      <div>
+        <form>
+          <label>Search:</label>
+          <input value={term} onChange={e => setTerm(e.target.value)}></input>
+        </form>
+      </div>
+      <h1>{term}</h1>
+    </>
+  )
 }
 
 export default App
